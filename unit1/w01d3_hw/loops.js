e27bfee3ddd423241ed1b47cd080bb002c1a59f6
Phi Huynh <huynh@Phis-MBP.fios-router.home>
2019-12-08 18:04:29 -0500

    finished loop practice

---
+++ unit1/w01d3_hw/loops.js
@@ -0,0 +1,35 @@
+//Create a loop that logs the numbers from 0-99 (ascending)
+
+for (let i = 0; i <= 99; i++) {
+    console.log(i)
+}
+
+//Create a loop that logs the numbers from 99-0 (descending)
+
+for (let i = 99; i >= 0; i--) {
+    console.log(i)
+}
+
+//Create a loop that logs the EVEN numbers from 0-98 (ascending)
+
+for (let i = 0; i <= 98; i++) {
+    if (i % 2 === 0) {
+    console.log(i)
+}
+};
+
+//Create a loop that logs the EVEN numbers from 98-0 (descending)
+
+for (let i = 98; i >= 0; i--) {
+    if (i % 2 === 0) {
+    console.log(i)
+}
+};
+
+//Create a loop that logs the ODD numbers from 0-99 (ascending)
+
+for (let i = 1; i <= 99; i++) {
+    if (i % 2){
+    console.log(i)
+}
+};
