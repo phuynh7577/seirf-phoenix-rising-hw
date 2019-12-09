1cde698ac0cad1c80786ff092f0f1ae6f24acf1c
Phi Huynh <huynh@Phis-MBP.fios-router.home>
2019-12-08 20:42:11 -0500

    w01d3 hw

--- unit1/w01d3_hw/loops.js
+++ unit1/w01d3_hw/loops.js
@@ -2,13 +2,13 @@
 
 for (let i = 0; i <= 99; i++) {
     console.log(i)
-}
+};
 
 //Create a loop that logs the numbers from 99-0 (descending)
 
 for (let i = 99; i >= 0; i--) {
     console.log(i)
-}
+};
 
 //Create a loop that logs the EVEN numbers from 0-98 (ascending)
 
@@ -30,6 +30,34 @@ for (let i = 98; i >= 0; i--) {
 
 for (let i = 1; i <= 99; i++) {
     if (i % 2){
+    console.log(i)
+}
+};
+
+//Create a loop that logs the ODD numbers from 99-0 (descending)
+
+for (let i = 99; i >= 0; i--) {
+    if (i % 2){
+    console.log(i)
+}
+};
+
+//Create a loop that logs the numbers from 49-72 (ascending)
+
+for (let i = 49; i <= 72; i++) {
+    console.log(i)
+};
+
+//Create a loop that logs the numbers from 81-26 (descending)
+
+for (let i = 81; i >= 26; i--) {
+    console.log(i)
+};
+
+//Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+
+for (let i = 3; i <= 90; i++) {
+    if (i % 3 === 0){
     console.log(i)
 }
 };
