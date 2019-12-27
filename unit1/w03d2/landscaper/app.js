edae363ac21e981c46d55f2552c83e3e3baa560e
Phi Huynh <huynh@Phis-MBP.fios-router.home>
2019-12-26 22:56:04 -0500

    finished hw

--- unit1/w03d2/landscaper/app.js
+++ unit1/w03d2/landscaper/app.js
@@ -1,52 +1,8 @@
-// window.confirm("Ready to run your own Landscaping Business?!!?");
-// if (confirm("Press a button!")) {
-//     txt = "You pressed OK!";
-//   } else {
-//     txt = "You pressed Cancel!";
-//   }
-
-
-// const myFunction = () => {
-//     if (balance === 5) {
-//     alert("You can now buy scissors for $5.")
-//     } if (balance === 25) {
-//     alert("You can now buy scissors for $25.")
-//     }if (balance === 250) {
-//     alert("You can now buy scissors for $250.")
-//     }if (balance === 500) {
-//     alert("You can now buy scissors for $500.")
-//     }if (balance === 1000) {
-//     alert("You now have $1000 and own a slave. YOU WIN")
-//     }
-//     document.getElementById("demo").innerHTML = txt;
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-let currentTool= '';
 let balance = [];
 let sum = 0;
 let totalBalance = sum;
 
-
-
-
-
-
-
-
-
-//start button
-
+//start 
   alert("Starting your first business can be challenging but, with persistence and hard work you will build a successful career!");
 
 
@@ -63,14 +19,14 @@ const cutWithTeeth = () => {
     sum += 1
     alert("Good work today! You earned $1.")
   }if (sum === 5) {
-    alert("You can buy a pair of siccors and earn $5 a day using them!")
+    alert("You can buy a pair of scissors and earn $5 a day using them!")
   }
 }
 
     
 //clicking scissors will add $5 to your balance
 const cutWithScissors = () => {
-  if (sum >= 5 && sum <25) {
+  if (sum >= 0 && sum < 25) {
     sum += 5 
     alert("Good work today! You earned $5.")
   }if (sum === 25) {
@@ -80,27 +36,27 @@ const cutWithScissors = () => {
 
 //clicking push mower will add $25 to your balance
 const cutWithPushMower = () => {
-  if (sum >= 25 && sum <250) {
+  if (sum >= 0 && sum < 250) {
     sum += 50
-    alert("Good work today! You earned $25.")
-  }if (sum >= 250) {
+    alert("Good work today! You earned $50.")
+  }if (sum === 250) {
     alert("You can now buy an electric mower and make $100 a day using it!")
   }
 }
 
 //clicking electric mower will add $100 to your balance
 const cutWithElectricMower = () => {
-  if (sum >= 250 && sum <=500) {
+  if (sum >= 0 && sum < 500) {
     sum += 100
     alert("Good work today! You earned $100.")
-  }if (sum >= 500) {
+  }if (sum === 500) {
     alert("Hard work pays off. Now you can hire a worker for $500 and make $250 a day!")
   }
 }
 
 //clicking the worker will add $250 to your balance
 const cutWithSlave = () => {
-  if (sum >= 500 && sum <=1000) {
+  if (sum >= 0 && sum < 1000) {
     sum += 250
     alert("Good work today! You earned $250.")
   }if (sum >= 1000) {
@@ -108,12 +64,20 @@ const cutWithSlave = () => {
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
-
- 
-
- 
+const buyInventory = () => {
+  if (sum >= 5 && sum < 25) {
+    sum -= 5;
+    alert ("You just added Scissors to your Inventory")
+  }if (sum >= 25 && sum < 250) {
+    sum -= 25;
+    alert ("You just added a push mower to your Inventory")
+}if (sum >= 250 && sum < 500) {
+  sum -= 250;
+  alert ("You just added an electric mower to your Inventory")
+}if (sum <= 500 && sum < 1000) {
+  sum -= 500;
+  alert ("You just hired a worker!")
+}
+}
